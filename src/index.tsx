0226c72ff48886deb688070bc440e42736020721
Matvei Morenkov <morenmat@fel.cvut.cz>
2024-05-24 21:44:53 +0200

    add marks to the map

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './map/MapComponent';
+import Map from './map/MapComponent';
 import reportWebVitals from './reportWebVitals';
+import Form from "./basemap/Form";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+
+    <Map />
+      <Form />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/map/MapComponent.tsx
+++ src/map/MapComponent.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import L from 'leaflet';
-import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
 import Basemap from "../basemap/Basemap";
 import './Map.css';
 
@@ -8,45 +8,152 @@ import 'leaflet/dist/leaflet.css';
 
 L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
 
-class MapComponent extends React.Component {
+interface MarkerData {
+    name: string;
+    description: string;
+    lat: number;
+    lng: number;
+}
 
-    state = {
+const customIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+const MapComponent: React.FC = () => {
+    const [state, setState] = useState({
         lat: 50.082,
         lng: 14.391,
         zoom: 14,
-        basemap: 'osm'
+        basemap: 'osm',
+        markers: [] as MarkerData[],
+        showModal: false,
+        markerLat: 0,
+        markerLng: 0,
+        markerName: "",
+        markerDescription: ""
+    });
+
+    useEffect(() => {
+        const savedMarkers = localStorage.getItem('markers');
+        if (savedMarkers) {
+            setState((prevState) => ({
+                ...prevState,
+                markers: JSON.parse(savedMarkers)
+            }));
+        }
+    }, []);
+
+    const onBMChange = (bm: string): void => {
+        setState((prevState) => ({
+            ...prevState,
+            basemap: bm
+        }));
     };
 
-    onBMChange = (bm: string): void => {
-        this.setState({
-            basemap: bm
+    const basemapsDict: { [key: string]: string } = {
+        osm: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        opnv: "https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png",
+        dark: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
+        cycle: "https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
+    };
+
+    const MapClickHandler = () => {
+        useMapEvents({
+            click: (e) => {
+                const { lat, lng } = e.latlng;
+                setState((prevState) => ({
+                    ...prevState,
+                    showModal: true,
+                    markerLat: lat,
+                    markerLng: lng
+                }));
+            }
         });
-    }
+        return null;
+    };
 
-    render() {
+    const handleCloseModal = () => {
+        setState((prevState) => ({
+            ...prevState,
+            showModal: false
+        }));
+    };
 
-        const basemapsDict: { [key: string]: string } = {
-            osm: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            opnv: "https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png",
-            dark: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
-            cycle: "https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
-        }
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setState((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
 
-        return (
-            <MapContainer center={[this.state.lat, this.state.lng]} zoom={this.state.zoom} scrollWheelZoom={true}>
-                <TileLayer
-                    // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url={basemapsDict[this.state.basemap]}
-                />
-                <Basemap basemap={this.state.basemap} onChange={this.onBMChange} />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br/> Easily customizable.
-                    </Popup>
-                </Marker>
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const { markerLat, markerLng, markerName, markerDescription } = state;
+        const newMarker: MarkerData = {
+            name: markerName,
+            description: markerDescription,
+            lat: markerLat,
+            lng: markerLng
+        };
+        setState((prevState) => ({
+            ...prevState,
+            markers: [...prevState.markers, newMarker],
+            showModal: false,
+            markerName: "",
+            markerDescription: ""
+        }));
+        localStorage.setItem('markers', JSON.stringify([...state.markers, newMarker]));
+    };
+
+    return (
+        <>
+            <MapContainer center={[state.lat, state.lng]} zoom={state.zoom} scrollWheelZoom={true}>
+                <TileLayer url={basemapsDict[state.basemap]} />
+                <Basemap basemap={state.basemap} onChange={onBMChange} />
+                <MapClickHandler />
+                {state.markers.map((marker, index) => (
+                    <Marker key={index} position={[marker.lat, marker.lng]} icon={customIcon}>
+                        <Popup>
+                            <div>
+                                <h3>{marker.name}</h3>
+                                <p>{marker.description}</p>
+                                <p>Latitude: {marker.lat}</p>
+                                <p>Longitude: {marker.lng}</p>
+                            </div>
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
-        );
-    }
-}
+            {state.showModal && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={handleCloseModal}>&times;</span>
+                        <p>Latitude: {state.markerLat}</p>
+                        <p>Longitude: {state.markerLng}</p>
+                        <form onSubmit={handleSubmit}>
+                            <label>
+                                WiFi Name:
+                                <input type="text" name="markerName" value={state.markerName} onChange={handleInputChange} />
+                            </label>
+                            <br />
+                            <label>
+                                Description:
+                                <textarea name="markerDescription" value={state.markerDescription} onChange={handleInputChange} />
+                            </label>
+                            <br />
+                            <button type="submit">Add WiFi Router</button>
+                        </form>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
 
 export default MapComponent;
